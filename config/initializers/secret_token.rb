b3484aacad423fad7740784377783e6130e4549c
Craig A. Cook <ccook@liveworld.com>
2012-09-22 14:15:56 -0700

    Initial generated Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NpiFinder::Application.config.secret_token = '5a282f073796397093ddf7adc81fa479326afd1d8dbb5ab8f45647dd921bfc06180db06e406576f534589d5a130f703adb4c3fed0113fb8b5b150e6da8eec2ef'
